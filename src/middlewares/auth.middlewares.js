bc463ee753d1d8c426d97f521e89697bcf8e89ef
tecni2 <tecni2.elie.elie@gmail.com>
2023-01-20 11:02:04 -0500

    error solucionado

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -3,18 +3,26 @@ require("dotenv").config();
 
 const authMiddleware = (req, res, next) => {
   let { authorization: token } = req.headers;
-  token = token.replace("Bearer ", "");
-  jwt.verify(token, process.env.JWT_SECRET, { algorithms: "HS512" }, (err, decoded) => {
-    if (err) {
-      res.status(400).json({
-        error: "invalid token",
-        message: "El token no es valido o ya expiro, envia uno correcto",
-      });
-    } else {
-      console.log(decoded);
-      next()
-    }
-  })
+  token = token?.replace("Bearer ", "");
+  if (token) {
+
+    jwt.verify(token, process.env.JWT_SECRET, { algorithms: "HS512" }, (err, decoded) => {
+      if (err) {
+        res.status(400).json({
+          error: "invalid token",
+          message: "El token no es valido o ya expiro, envia uno correcto",
+        });
+      } else {
+        console.log(decoded);
+        next()
+      }
+    })
+  } else {
+    res.status(400).json({
+      error: "no token provided",
+      message: "No se esta enviando un token de autenticacion"
+    })
+  }
   // next();
 }
 
